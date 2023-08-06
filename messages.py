fa559a9445350f9bd4971083bc015a7287c10348
Gustavo Isoo Binder <gustan.binder@gmail.com>
2023-08-06 10:55:54 -0300

    add: message repository

---
+++ messages.py
@@ -0,0 +1,14 @@
+intro = [
+    "On a lovely summer morning,",
+    "Arthur woke up excited to visit Fenoxer.",
+    "However, after quick inspection...",
+    "He noticed his Eevee was GONE!!!",
+    "Scared, Arthur went looking for her."
+]
+
+villager_1 = [
+    "Hello There, Arthur.",
+    "I've heard that your Eevee is gone!",
+    "You must be horrified.",
+    "I hope you find it soon..."
+]
