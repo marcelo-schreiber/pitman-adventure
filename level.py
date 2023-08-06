0e8a92e35b79e73689b7cb3766a40edb27c57fbe
Gustavo Isoo Binder <gustan.binder@gmail.com>
2023-08-06 10:54:59 -0300

    feat: integrating textbox and npc classes

--- level.py
+++ level.py
@@ -5,6 +5,10 @@ from player import Player
 from utils import import_csv_layout, import_folder
 import random
 
+from textbox import *
+from npc import Npc
+from messages import *
+
 
 class Level:
     def __init__(self):
@@ -16,26 +20,36 @@ class Level:
         self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
         self.grass_sprites = pygame.sprite.Group()  # for random encounter
+        self.npc_sprites = pygame.sprite.Group()
 
         # sprite setup
         self.create_map()
 
         self.player = None
         self.initialize_player()
+        
+        self.s1 = Textbox()
+        self.s1.start_text(intro)
+
+        self.create_npc()
 
     def initialize_player(self):
-        player_initial_x = TILESIZE * 10
-        player_initial_y = TILESIZE * 6
+        player_initial_x = TILESIZE * 11
+        player_initial_y = TILESIZE * 11
 
         self.player = Player((player_initial_x, player_initial_y),
-                             [self.visible_sprites], self.obstacle_sprites, self.grass_sprites)
+                             [self.visible_sprites], self.obstacle_sprites, self.grass_sprites, self.npc_sprites)
+    
+    def create_npc(self):
+        groups = [self.visible_sprites, self.npc_sprites]
+        Npc(groups, (9, 10), 'spirit', villager_1)
 
     def create_map(self):
         layouts = {  # list of layouts (collision, visuals, etc)
             'boundary': import_csv_layout('map/map_FloorBlocks.csv'),
             'grass': import_csv_layout('map/map_Grass.csv'),
             'object': import_csv_layout('map/map_LargeObjects.csv'),
-            'details': import_csv_layout('map/map_Details.csv'),
+            'details': import_csv_layout('map/map_Details.csv')
         }
 
         graphics = {  # list of images
@@ -53,18 +67,25 @@ class Level:
 
                     if style == 'boundary':
                         Tile((x, y), [self.obstacle_sprites], 'boundary')
-                    if style == 'object':
-                        Tile((x, y), [self.obstacle_sprites], 'object')
+                    # if style == 'object':
+                    #     Tile((x, y), [self.obstacle_sprites], 'object')
 
-                    if style == 'grass':
-                        random_grass_surface = random.choice(graphics['grass'])
-                        Tile((x, y), [self.visible_sprites, self.grass_sprites], 'grass', random_grass_surface)
+                    # if style == 'grass':
+                    #     random_grass_surface = random.choice(graphics['grass'])
+                    #     Tile((x, y), [self.visible_sprites, self.grass_sprites], 'grass', random_grass_surface)
+
+                    # if style == 'npcs':
+                    #     Npc([self.visible_sprites, self.npc_sprites], (x, y))
 
     def run(self):
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
 
-        self.visible_sprites.update()
+        if self.s1.active:
+            self.s1.draw()
+            self.s1.update()
+        else:
+            self.visible_sprites.update()
 
 
 class YSortCameraGroup(pygame.sprite.Group):
